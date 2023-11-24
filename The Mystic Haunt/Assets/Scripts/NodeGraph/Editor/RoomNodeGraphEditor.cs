7a74e8d148ea17f4ffc8c29ad2945fb08335db3e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-24 10:37:14 +0000

    Create game resources class

--- The Mystic Haunt/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
+++ The Mystic Haunt/Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEditor;
 using UnityEngine;
 
@@ -12,11 +10,27 @@ public class RoomNodeGraphEditor : EditorWindow
     private const int nodeBorder = 12;
     
     private GUIStyle roomNodeStyle;
+    private static RoomNodeGraphSO currentRoomNodeGraph;
+    private RoomNodeTypeListSO roomNodeTypeList;
     
     [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
     static void OpenWindow()
     {
         GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
+
+    [OnOpenAsset(0)]
+    public static bool OnDoubleClickAsset(int instanceID, int line)
+    {
+        RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
+        if (roomNodeGraph != null)
+        {
+            OpenWindow();
+            currentRoomNodeGraph = roomNodeGraph;
+            return true;
+        }
+
+        return false;
     }
 
     private void OnEnable()
@@ -26,17 +40,60 @@ public class RoomNodeGraphEditor : EditorWindow
         roomNodeStyle.normal.textColor = Color.white;
         roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
         roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
-        
+
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
     }
 
     private void OnGUI()
     {
-        GUILayout.BeginArea(new Rect(new Vector2(100f,100f),new Vector2(nodeWidth,nodeHeight)),roomNodeStyle);
-        EditorGUILayout.LabelField("Node 1");
-        GUILayout.EndArea();
-        
-        GUILayout.BeginArea(new Rect(new Vector2(300f,300f),new Vector2(nodeWidth,nodeHeight)),roomNodeStyle);
-        EditorGUILayout.LabelField("Node 2");
-        GUILayout.EndArea();
+        if (currentRoomNodeGraph != null)
+        {
+            ProcessEvents(Event.current);
+
+           // DrawRoomNodes();
+        }
+
+        if (GUI.changed)
+            Repaint();
+
+        // GUILayout.BeginArea(new Rect(new Vector2(100f,100f),new Vector2(nodeWidth,nodeHeight)),roomNodeStyle);
+        // EditorGUILayout.LabelField("Node 1");
+        // GUILayout.EndArea();
+        //
+        // GUILayout.BeginArea(new Rect(new Vector2(300f,300f),new Vector2(nodeWidth,nodeHeight)),roomNodeStyle);
+        // EditorGUILayout.LabelField("Node 2");
+        // GUILayout.EndArea();
+    }
+
+    void ProcessEvents(Event currentEvent)
+    {
+        ProcessRoomNodeGraphEvents(currentEvent);
+    }
+
+    void ProcessRoomNodeGraphEvents(Event currentEvent)
+    {
+        switch (currentEvent.type)
+        {
+            case EventType.MouseDown:
+                ProcessMouseDownEvent(currentEvent);
+                break;
+            default: 
+                break;
+        }
+    }
+
+    void ProcessMouseDownEvent(Event currentEvent)
+    {
+        if (currentEvent.button == 1)
+        {
+            ShowContextMenu(currentEvent.mousePosition);
+        }
+    }
+
+    void ShowContextMenu(Vector2 mousePosition)
+    {
+        GenericMenu menu = new GenericMenu();
+        //menu.AddItem(new GUIContent("Create Room Node"),false,CreateRoomNode,mousePosition);
+        menu.ShowAsContext();
     }
 }
